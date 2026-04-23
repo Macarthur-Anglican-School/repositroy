054c458d18fc2599c9d7c5a1388caf6746a3bdb9
R1cecracker <troy.harcoan0@gmail.com>
2026-04-23 21:27:44 +1000

    trying to get service worker to work

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,11 +1,28 @@
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.open('my-pwa-cache')
-            .then(cache => {
-                return cache.match(event.request)
-                    .then(response => {
-                        return response || fetch(event.request);
-                    });
-            })
-    );
+const CACHE_NAME = "cyberwatch-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/static/css/style.css",
+  "/static/js/script.js",
+  "/static/icons/icon-192.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request).catch(() => {
+          return caches.match("/offline.html");
+        });
+      })
+  );
 });
